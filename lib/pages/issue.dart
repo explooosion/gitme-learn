c8d1b050b814c9c9c4865a6e5be59ad166bc774b
explooosion <ta7382@gmail.com>
2020-01-25 14:48:47 +0800

    Update issue with fetch github

--- lib/pages/issue.dart
+++ lib/pages/issue.dart
@@ -1,44 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:github/github.dart';
+import 'package:my_app/services/github_api.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
-class IssuePage extends StatelessWidget {
-  final List repoList = [
-    {
-      "title": "BbsonLin/gitme_reborn",
-      "description": "No description provided.\n\n★ 0",
-      "lang": "● Dart"
-    },
-    {
-      "title": "BbsonLin/ithome-ironman",
-      "description": "No description provided.\n\n★ 0",
-      "lang": ""
-    },
-    {
-      "title": "BbsonLin/flask-request-logger",
-      "description":
-          "A Flask extension for recording requests and responses into database\n\n★ 3",
-      "lang": "● Python"
-    },
-  ];
+class IssuePage extends StatefulWidget {
+  _IssuePageState createState() => _IssuePageState();
+}
+
+class _IssuePageState extends State<IssuePage> {
+  Future<List<Issue>> issueList;
+
+  @override
+  void initState() {
+    super.initState();
+    issueList = fetchIssues();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.separated(
-        itemCount: repoList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text(repoList[index]["title"]),
-            subtitle: Text(repoList[index]["description"]),
-            trailing: Text(repoList[index]["lang"]),
-            isThreeLine: false,
-            contentPadding: EdgeInsets.all(16),
-            onTap: () {},
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return Divider(height: 8);
+    return Scrollbar(
+      child: RefreshIndicator(
+        child: FutureBuilder(
+          future: issueList,
+          builder: (BuildContext context, AsyncSnapshot<List<Issue>> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                if (!snapshot.hasError) {
+                  return ListView.separated(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final now = DateTime.now();
+                      final difference =
+                          now.difference(snapshot.data[index].createdAt);
+                      var createTimeAgo =
+                          timeago.format(now.subtract(difference));
+
+                      return ListTile(
+                        dense: true,
+                        leading: CircleAvatar(
+                          radius: 18.0,
+                          backgroundImage:
+                              NetworkImage(snapshot.data[index].user.avatarUrl),
+                        ),
+                        title: Text(snapshot.data[index].title),
+                        subtitle: Text(createTimeAgo),
+                        trailing: Icon(Icons.error_outline),
+                        onTap: () {},
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) =>
+                        const Divider(height: 0.0),
+                  );
+                } else {
+                  return Center(child: Text("No Data"));
+                }
+                break;
+              case ConnectionState.none:
+              case ConnectionState.active:
+              case ConnectionState.waiting:
+                return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+        onRefresh: () async {
+          await Future.delayed(Duration(seconds: 1));
+          setState(() {
+            issueList = fetchIssues();
+          });
         },
       ),
     );
+  }
+
+  Future<List<Issue>> fetchIssues() async {
+    return githubClient.issues.listAll(state: "all").toList();
   }
 }
